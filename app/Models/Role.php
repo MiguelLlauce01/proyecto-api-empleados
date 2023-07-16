1ea2be15ee1422e5fe1795159af0b9a7af652043
NayelliGS18 <ngayosos@unprg.edu.pe>
2023-07-16 17:03:09 -0500

    crud roles

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+
+    //Se agregó este código
+    protected $fillable = ["rol", "state"];
+
+    public function getEmployee()//Función para recuperar a todos los empleados activos
+    {
+        return $this->hasMany(Employee::class, 'rol_id','id')
+        ->where('state','ACTIVE');
+    }
 }
